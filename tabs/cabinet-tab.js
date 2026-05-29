918c797113810fd9074148cb0a5e10099832b594
iamloganw2007 <iamloganw2007@gmail.com>
2026-05-29 22:54:02 +0200

    Create cabinet-tab.js

---
+++ tabs/cabinet-tab.js
@@ -0,0 +1,9 @@
+window.WAHLKAMPF_TABS = window.WAHLKAMPF_TABS || {};
+
+window.WAHLKAMPF_TABS.cabinet = String.raw`
+      <div class="game-tab-view" id="game-view-cabinet">
+        <div class="game-governance-view">
+          <div id="game-gov-host-cabinet" class="game-governance-host"></div>
+        </div>
+      </div>
+`;
